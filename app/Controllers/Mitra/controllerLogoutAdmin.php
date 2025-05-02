62702f500625bd5cd8a50335e245e9fd4503ecf1
unknown <allmer.djibran.2405356@students.um.ac.id>
2025-05-02 11:03:51 +0700

    update 11:03/2/5/2025

--- app/Controllers/Mitra/controllerLogoutAdmin.php
+++ app/Controllers/Mitra/controllerLogoutAdmin.php
@@ -5,7 +5,7 @@ use App\Controllers\BaseController;
 
 class controllerLogoutAdmin extends BaseController{
     public function logOut(){
-        session()->destroy();
+        session()->remove('isLoggedInAdmin', 'Username', 'ID_Admin');
         return redirect()->to('Mitra/controllerLoginAdmin')->with('success', 'Akun Telah Logout');
     }
 }
--- app/Controllers/Pelanggan/controllerLogoutAkunPelanggan.php
+++ app/Controllers/Pelanggan/controllerLogoutAkunPelanggan.php
@@ -5,7 +5,7 @@ use App\Controllers\BaseController;
 
 class controllerLogoutAkunPelanggan extends BaseController{
     public function logOut(){
-        session()->destroy();
+        session()->remove('isLoggedIn', 'Email', 'ID_User', 'Nama_Depan');
         return redirect()->to('Pelanggan/controllerHomepage')->with('success', 'Akun Telah Logout');
     }
 }
